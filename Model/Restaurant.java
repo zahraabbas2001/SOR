dbee41092cc09aab80e6d1c41f9f40f1d080637e
BUseKucukbukucu <119704645+BUseKucukbukucu@users.noreply.github.com>
2022-12-16 20:20:00 +0300

    Update Restaurant.java

--- Model/Restaurant.java
+++ Model/Restaurant.java
@@ -2,6 +2,19 @@ package Model;
 
 public class Restaurant {
 
-
+private ArrayList<Employee> employees = new ArrayList<> ();
+private ArrayList<Product> products= new ArrayList();
+  
+  
+  public Restaurant (){
+    initEmployees();
+    initProducts();
+    
+  }
+    
+  private void initEmployees(){
+  //add employees here
+  }
+  
 }
 
