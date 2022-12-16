71cf94aae502e085934746563e8417d1188c61f3
BUseKucukbukucu <119704645+BUseKucukbukucu@users.noreply.github.com>
2022-12-16 19:40:00 +0300

    Update Employee.java

--- Model/Employee.java
+++ Model/Employee.java
@@ -5,6 +5,20 @@ public class Employee implements Expense {
   private static final AtomicInteger count= new AtomicInteger(0);
   private int id;
   private String name;
+   
+   public Employee (int id, String name){
+      this.id=count.incrementAndGet();
+      this.name=name;
+      
+   }
+   
+   public Employee()
+   {
+      id=count.incrementAndGet();
+      name=" ";
+   }
+   
+   public void setName()
   
   
 }
