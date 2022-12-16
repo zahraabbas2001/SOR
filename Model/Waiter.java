9073a173b211fcc344ebb6167bd5d45e8e038c67
murtaza215 <101196672+murtaza215@users.noreply.github.com>
2022-12-16 19:42:17 +0300

    added constructor

--- Model/Waiter.java
+++ Model/Waiter.java
@@ -2,8 +2,15 @@ package Model;
 
 import java.util.ArrayList;
 
-public class Waiter {
-
+public class Waiter extends Employee {
+private double orderRate;
+	private ArrayList<Order> ordersRecieved;
+	public Waiter(int id, String name) 
+	{
+		super(id,name);
+		this.orderRate = 0.10;
+		ordersRecieved = new ArrayList<>();
+	}
 
 
 }
