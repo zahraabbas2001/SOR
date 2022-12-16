5005803e5f30a48b147d48040c55671c432ff19e
Zahra Abbas <zahra.abbas@ozu.edu.tr>
2022-12-16 19:48:09 +0300

    Main Dish class

---
+++ Model/MainDish.java
@@ -0,0 +1,11 @@
+package Model;
+
+public class MainDish extends Product {
+    public MainDish(String name, double sellingPrice, double purchasePrice, double utilityCost) {
+        super(name,sellingPrice,purchasePrice,utilityCost);
+    }
+    public double calculateExpense() {
+        return super.getPurchasePrice() + super.getUtilityCost();
+    }
+
+}
