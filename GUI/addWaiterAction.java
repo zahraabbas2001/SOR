12fb58a13c5fa6809fb3aa2a1944833839080ae2
BUseKucukbukucu <119704645+BUseKucukbukucu@users.noreply.github.com>
2022-12-21 20:52:05 +0300

    Update addWaiterAction.java

--- GUI/addWaiterAction.java
+++ GUI/addWaiterAction.java
@@ -1,5 +1,3 @@
-package GUI;
-
 import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -9,8 +7,28 @@ import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
 
-import Model.Restaurant;
+public class AddWaiterAction  implements ActionListener {
+    private Restaurant restaurant;
 
-public class AddWaiterAction implements ActionListener {
-    
+    public AddWaiterAction(Restaurant restaurant) {
+        this.restaurant = restaurant;
+    }
+
+    public void actionPerformed(ActionEvent w) {
+        JPanel panel = new JPanel();
+        panel.setLayout(new GridLayout(2, 2));
+        JTextField nameField = new JTextField();
+        JButton addButton = new JButton("Add");
+        panel.add(new JLabel("Name:"));
+        panel.add(nameField);
+        panel.add(addButton);
+
+        addButton.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                String name = nameField.getText();
+                Waiter waiter = new Waiter(name);
+                restaurant.addWaiter(waiter);
+            }
+        });
+    }
 }
