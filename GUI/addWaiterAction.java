0b0d366a191be1f8a44e206c76e66b49a4acacb8
BUseKucukbukucu <119704645+BUseKucukbukucu@users.noreply.github.com>
2022-12-22 22:00:47 +0300

    Update addWaiterAction.java
    added salary info to the waiter

--- GUI/addWaiterAction.java
+++ GUI/addWaiterAction.java
@@ -9,25 +9,32 @@ import javax.swing.JTextField;
 
 public class AddWaiterAction  implements ActionListener {
     private Restaurant restaurant;
+    private RestaurantPanel restaurantPanel;
 
-    public AddWaiterAction(Restaurant restaurant) {
+    public AddWaiterAction(RestaurantPanel restaurantPanel,Restaurant restaurant) {
+        this.restaurantPanel=restaurantPanel;
         this.restaurant = restaurant;
     }
 
     public void actionPerformed(ActionEvent w) {
         JPanel panel = new JPanel();
-        panel.setLayout(new GridLayout(2, 2));
+        panel.setLayout(new GridLayout(3, 2));
         JTextField nameField = new JTextField();
+        JTextField salaryField= new JTextField();
         JButton addButton = new JButton("Add");
         panel.add(new JLabel("Name:"));
         panel.add(nameField);
+        panel.add(new JLabel("Salary:"));
+        panel.add(salaryField);
         panel.add(addButton);
 
         addButton.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent e) {
                 String name = nameField.getText();
-                Waiter waiter = new Waiter(name);
+                double salary=Double.parseDouble(salaryField.getText());
+                Waiter waiter = new Waiter(name,salary);
                 restaurant.addWaiter(waiter);
+                //update Waiter List??
             }
         });
     }
