28e2f5b1503c84f6fee1983ddbfebfda27c64dd9
CanCan93 <120737097+CanCan93@users.noreply.github.com>
2022-12-22 19:25:47 +0000

    modified:   GUI/ListEmployeeAction.java

--- GUI/ListEmployeeAction.java
+++ GUI/ListEmployeeAction.java
@@ -11,5 +11,19 @@ import javax.swing.JTable;
 import Model.Restaurant;
 
 public class CalculateExpenseAction implements ActionListener{
-    
+    private RestaurantPanel restaurantPanel;
+
+    public ListEmployeeAction(RestaurantPanel restaurantPanel){
+        this.restaurantPanel = restaurantPanel;
+    }
+    @Override
+    public void actionPerformed(ActionEvent e){
+        JPanel panel = new JPanel();
+        panel.setLayout(new GridLayout(restaurantPanel.getRestaurant().getEmployees().size()+1,3));
+        for(int i = 0; i<restaurantPanel.getRestaurant().getEmployees().size(); i++){
+            panel.add(new JLabel(restaurantPanel.getRestaurant().getEmployees().get(i).getName()));
+        }
+        panel.setBounds(200,900,200,900);
+        restaurantPanel.add(panel);
+    }
 }
