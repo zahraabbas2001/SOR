96275729d89db84349f1cd048df4dc7a20f4953b
murtaza215 <101196672+murtaza215@users.noreply.github.com>
2022-12-22 22:18:27 +0300

    added initproducts function

--- Model/Restaurant.java
+++ Model/Restaurant.java
@@ -26,3 +26,32 @@ private ArrayList<Product> products= new ArrayList();
 			System.out.println(employee);
 		}
 	}
+	private void initProducts()
+	{
+
+		// Parameters for Product Constructor:
+		// Product Name, Selling Price, Purchase Price and Utility Cost
+		
+		products.add(new MainDish("Pizza", 6, 2, 2));
+		products.add(new MainDish("Burger", 5, 1.5, 2));
+		
+		products.add(new Beverage("Coke", 2, 0.5));
+		products.add(new Beverage("Lemonade", 2, 0.3));
+		
+		products.add(new Dessert("Tiramusu", 4, 1, 1));
+		products.add(new Dessert("Cake", 3, 0.5, 1));
+		products.add(new Dessert("Ice Cream", 3, 0.5, 0.5));
+		
+		ArrayList<Product> HGproducts = new ArrayList<>();
+		HGproducts.add(new MainDish("Pizza", 6, 2, 2));
+		HGproducts.add(new Beverage("Coke", 2, 0.5));
+		HGproducts.add(new Dessert("Tiramusu", 4, 1, 1));
+		products.add(new MenuProduct("Hunger Games Menu", HGproducts));
+		
+		ArrayList<Product> Kidsproducts = new ArrayList<>();
+		Kidsproducts.add(new MainDish("Burger", 5, 1.5, 2));
+		Kidsproducts.add(new Beverage("Lemonade", 2, 0.3));
+		Kidsproducts.add(new Dessert("Ice Cream", 3, 0.5, 0.5));
+		products.add(new MenuProduct("Kids Menu", Kidsproducts));
+	}
+	
