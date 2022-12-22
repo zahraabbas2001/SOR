b74a5729de58a9eebd143f40c9f1790519274bf6
CanCan93 <120737097+CanCan93@users.noreply.github.com>
2022-12-22 19:04:25 +0000

    modified:   GUI/addCookAction.java

--- GUI/addCookAction.java
+++ GUI/addCookAction.java
@@ -11,6 +11,26 @@ import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
 
-public class AddCookAction extends Jpanel {
+import Model.Restaurant;
 
+public class AddCookAction extends Jpanel {
+    Restaurant restaurant;
+    public AddCookAction(Resaurant restaurant){
+        this.restaurant = restaurant;
+        setLayout(new BorderLayout());
+    }
+    public void actionPerformed(ActionEvent e){
+        JWindows window = new Window();
+        window.setLayout(new GridLayout(5,2));
+        JTextField name = new JTextField();
+        window.add(name);
+        JFormattedTextField salary = new JFormattedTextField(NumberFormat.getNumberInstance());
+        window.add(salary);
+        JButton add = new Button("Add");
+        window.add(new JLable("Name= "));
+        window.add(name);
+        window.add( new JLanle("Salary= "));
+        window.add(salary);
+        window.add(add);
+    }
 }
