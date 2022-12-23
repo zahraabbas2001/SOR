8d7d9195d09c98f828747039e3dadaa07d35fbf5
FaridGahramanov <101170007+FaridGahramanov@users.noreply.github.com>
2022-12-23 20:23:52 +0400

    Updated Beverage.java

--- Model/Beverage.java
+++ Model/Beverage.java
@@ -1,12 +1,13 @@
-package Model;
+package restaurantManagementSystem;
 
 public class Beverage extends Product {
-  
-  public Beverage(String name, double sellingPrice, double purchasePrice) {
-    super(name);
-    super.setSellingPrice(sellingPrice);
-    super.setPurchasePrice(purchasePrice);
-  }
-  public double calculateExpense() { return super.getPurchasePrice(); }
-  
+    public Beverage(String name, double sellingPrice, double purchasePrice) {
+        super(name);
+        super.setSellingPrice(sellingPrice);
+        super.setPurchasePrice(purchasePrice);
+    }
+
+    public double calculateExpense() {
+        return super.getPurchasePrice();
+    }
 }
