12129bd4fdbc370c7bb464382cb780638f561c66
FaridGahramanov <101170007+FaridGahramanov@users.noreply.github.com>
2022-12-23 20:28:19 +0400

    Updated Cook.java

--- Model/Cook.java
+++ Model/Cook.java
@@ -1,38 +1,33 @@
-package Model;
+package restaurantManagementSystem;
 
 public class Cook extends Employee {
-	private double salary;
-	private double taxRate;
-	public Cook(int id, String name, double salary)
-	{
-		super(id,name);
-		this.salary = salary;
-		this.taxRate = 0.18;
-	}
-	public Cook()
-	{
-	  super();
-	  salary = 0.0;
-	  taxRate = 0.18;
-	}
-	public void setSalary(double salary) 
-	{
-		this.salary = salary;
-	}
-	//Getter functions
-	public double getSalary()
-	{
-		return salary;
-	}
-	public double getTaxRate()
-	{
-		return taxRate;
-	}
-	public double calculateExpense() 
-	{
-		//System.out.println("salary: " + this.getSalary()+"\t taxRate: "+(this.getSalary() * this.getTaxRate()));
-		return this.getSalary() + (this.getSalary() * this.getTaxRate());
-	
-	}
+    private double salary;
+    private double taxRate;
 
+    public Cook(int id, String name, double salary) {
+        super(id, name);
+        this.salary = salary;
+        this.taxRate = 0.18;
+    }
+
+    public Cook() {
+        this.salary = 0.0;
+        this.taxRate = 0.18;
+    }
+
+    public void setSalary(double salary) {
+        this.salary = salary;
+    }
+
+    public double getSalary() {
+        return this.salary;
+    }
+
+    public double getTaxRate() {
+        return this.taxRate;
+    }
+
+    public double calculateExpense() {
+        return this.getSalary() + this.getSalary() * this.getTaxRate();
+    }
 }
