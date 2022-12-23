592d3539236dfcf59d5166a2d51a65f32ec36be5
murtaza215 <101196672+murtaza215@users.noreply.github.com>
2022-12-23 13:52:46 +0300

    class constructor

--- Model/MainDish.java
+++ Model/MainDish.java
@@ -1,8 +1,5 @@
 public class MainDish extends Product {
-    public MainDish(String name, double sellingPrice, double purchasePrice, double utilityCost) {
-        super(name,sellingPrice,purchasePrice,utilityCost);
-    }
-    public double calculateExpense() {
-        return super.getPurchasePrice() + super.getUtilityCost();
-    }
+public MainDish(String name, double sellingPrice, double purchasePrice, double utilityCost) {
+		super(name,sellingPrice,purchasePrice,utilityCost);
+	}
 }
