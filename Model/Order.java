811b56c12e375a363d17a71096ef42109b643f12
murtaza215 <101196672+murtaza215@users.noreply.github.com>
2022-12-23 13:42:34 +0300

    constructor, addProduct and listOrder function

--- Model/Order.java
+++ Model/Order.java
@@ -2,50 +2,29 @@ package Model;
 
 import java.util.ArrayList;
 
-public class Order
+public class Order 
 {
-    private ArrayList<Product> orderedProducts;
+	public ArrayList<Product> orderedProducts;
+	
+	public Order() 
+	{
+		orderedProducts = new ArrayList<>();
+	}
+	public void addProduct(Product product) {
+		orderedProducts.add(product);
+	}
+	public void listOrder() 
+	{
+		if(orderedProducts.size()==0)
+		{
+			System.out.println("You have not ordered anything yet");
+		}
+		else 
+		{
+			for (Product orderedProduct: orderedProducts)
+			{
+				System.out.println(orderedProduct);
+			}
+		}
+	}
 
-    public Order()
-    {
-        orderedProducts = new ArrayList<>();
-    }
-    public void addProduct(Product product) {
-        orderedProducts.add(product);
-    }
-    public void listOrder()
-    {
-        if(orderedProducts.size()==0)
-        {
-            System.out.println("You have not ordered anything yet");
-        }
-        else
-        {
-            for (Product orderedProduct: orderedProducts)
-            {
-                System.out.println(orderedProduct);
-            }
-        }
-    }
-    public ArrayList<Product> getOrderedProducts()
-    {
-        return orderedProducts;
-    }
-    public double calculateTotalPrice()
-    {
-        double totalPrice = 0.0;
-        for(int i = 0 ; i< orderedProducts.size() ; i++)
-        {
-            totalPrice += orderedProducts.get(i).getSellingPrice();
-        }
-        return totalPrice;
-    }
-    public double calculateOrderExpenses() {
-        double orderExpense = 0.0;
-        for(Product orderedProduct: orderedProducts) {
-            orderExpense += orderedProduct.calculateExpense();
-        }
-        return orderExpense;
-    }
-
-}
