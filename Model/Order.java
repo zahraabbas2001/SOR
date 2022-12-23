6e9349b473c540c2d14aca71a6a66b82aa269da4
murtaza215 <101196672+murtaza215@users.noreply.github.com>
2022-12-23 13:43:37 +0300

    getOrderedProducts and calculateTotalPrice

--- Model/Order.java
+++ Model/Order.java
@@ -26,5 +26,18 @@ public class Order
 				System.out.println(orderedProduct);
 			}
 		}
+		public ArrayList<Product> getOrderedProducts()
+	{
+		return orderedProducts;
+	}
+	public double calculateTotalPrice()
+	{
+		double totalPrice = 0.0;
+		for(int i = 0 ; i< orderedProducts.size() ; i++) 
+		{
+			totalPrice += orderedProducts.get(i).getSellingPrice();
+		}
+		return totalPrice;
+	}
 	}
 
