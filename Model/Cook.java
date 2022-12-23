5e88247804d2460ba95b4bcc96ad2446aaddeafa
murtaza215 <101196672+murtaza215@users.noreply.github.com>
2022-12-23 13:58:03 +0300

    getter, setter and calculateExpense functions

--- Model/Cook.java
+++ Model/Cook.java
@@ -15,3 +15,24 @@ public class Cook extends Employee {
 	  salary = 0.0;
 	  taxRate = 0.18;
 	}
+	public void setSalary(double salary) 
+	{
+		this.salary = salary;
+	}
+	//Getter functions
+	public double getSalary()
+	{
+		return salary;
+	}
+	public double getTaxRate()
+	{
+		return taxRate;
+	}
+	public double calculateExpense() 
+	{
+		//System.out.println("salary: " + this.getSalary()+"\t taxRate: "+(this.getSalary() * this.getTaxRate()));
+		return this.getSalary() + (this.getSalary() * this.getTaxRate());
+	
+	}
+
+}
