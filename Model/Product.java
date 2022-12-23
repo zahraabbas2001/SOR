849b8f1de7c19ffb6c3a905d6bc14935ee1eaab7
Zahra Abbas <zahra.abbas@ozu.edu.tr>
2022-12-23 19:27:29 +0300

    Updated the Product class

--- Model/Product.java
+++ Model/Product.java
@@ -1,62 +1,60 @@
 package Model;
 
-public class Product implements Expense
-{	private String name;
+public abstract class Product implements Expense {
+	private String name;
 	private double purchasePrice;
 	private double sellingPrice;
 	private double utilityCost;
-	public Product(String n , double s , double p, double u)
-	{
-		name = n;
-		sellingPrice = s;
-		purchasePrice = p;
-		utilityCost = u;
-	}
-	public Product(String n) {
-		name = n;
-	}
-	//Getter Functions
-	public String getName()
-	{
-		return name;
-	}
-	public double getPurchasePrice()
-	{
-		return purchasePrice;
-	}
-	public double getSellingPrice()
-	{
-		return sellingPrice;
-	}
-	public double getUtilityCost()
-	{
-		return utilityCost;
-	}
-	//Setter Functions
-	public void setName(String name)
-	{
+
+
+
+
+	public Product(String name, double purchasePrice, double sellingPrice, double utilityCost) {
+
 		this.name = name;
-	}
-	public void setSellingPrice(double sellingPrice)
-	{
-		this.sellingPrice = sellingPrice ;
-	}
-	public void setPurchasePrice(double purchasePrice) 
-	{
 		this.purchasePrice = purchasePrice;
-	}
-	public void setUtilityCost(double utilityCost) {
+		this.sellingPrice = sellingPrice;
 		this.utilityCost = utilityCost;
 	}
-	public String toString()
-	{
-		return this.name + ": " + this.sellingPrice;
+	public void setPurchasePrice(double purchasePrice) {
+		this.purchasePrice = purchasePrice;
 	}
-	@Override
-	public double calculateExpense() {
-		// TODO Auto-generated method stub
-		return 0;
+	public Product(String name) {
+
+		this.name= name;
 	}
 
+	public String getName() {
+		return name;
+	}
+
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+
+	public double getPurchasePrice() {
+		return purchasePrice;
+	}
+
+
+
+	public double getSellingPrice() {
+		return sellingPrice;
+	}
+
+
+	public void setSellingPrice(double sellingPrice) {
+		this.sellingPrice = sellingPrice;
+	}
+
+	public double getUtilityCost() {
+		return utilityCost;
+	}
+
+	public String toString() {
+		return " Product Name: " +name+ "  ";
+	}
 }
 
