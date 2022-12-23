25cac1471e528f14454e4713e7d76d9a1b9f4f4a
murtaza215 <101196672+murtaza215@users.noreply.github.com>
2022-12-23 13:57:16 +0300

    Added parameterized and default constructor

--- Model/Cook.java
+++ Model/Cook.java
@@ -1,24 +1,17 @@
-public class Cook extends Employee{
+package Model;
 
-    private double salary;
-    private double taxRate;
-
-public Cook(int id, String name, double salary){
-    super(id, name);
-    this.salary = salary;
-    this.taxRate = 0.18;
-}
-
-public double getSalary() {
-    return salary;
-}
-
-public double getTaxRate() {
-    return taxRate;
-}
-
-public double calculateExpense() {
-    return this.salary*(1+this.taxRate);
-}
-
-}
+public class Cook extends Employee {
+	private double salary;
+	private double taxRate;
+	public Cook(int id, String name, double salary)
+	{
+		super(id,name);
+		this.salary = salary;
+		this.taxRate = 0.18;
+	}
+	public Cook()
+	{
+	  super();
+	  salary = 0.0;
+	  taxRate = 0.18;
+	}
