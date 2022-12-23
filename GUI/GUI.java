176b8aaa3b9abc8f26911d346ae92616ccd941d9
CanCan93 <120737097+CanCan93@users.noreply.github.com>
2022-12-23 14:37:39 +0000

    modified:   GUI/GUI.java

--- GUI/GUI.java
+++ GUI/GUI.java
@@ -6,8 +6,34 @@ import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.JTabbedPane;
 
-import Model.Restaurant;
+import restaurantManagementSystem.Restaurant;
 
 public class Gui {
-    
+
+	public static void main(String[] args) {
+		Restaurant restaurant = new Restaurant();
+		
+		JFrame frame = new JFrame("Project");
+		
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setSize(650, 500);
+		frame.setMinimumSize(new Dimension(650, 500));
+		
+		JTabbedPane tabbedPane = new JTabbedPane();
+		
+		JPanel orderPanel = new OrderPanel(restaurant);
+		tabbedPane.add("Order", orderPanel);
+		
+		JPanel restaurantPanel = new RestaurantPanel(restaurant);
+		tabbedPane.add("Restaurant", restaurantPanel);
+
+
+		
+		frame.add(tabbedPane);
+		
+		frame.pack();
+		frame.setLocationRelativeTo(null);
+		frame.setVisible(true);
+	}
 }
+
